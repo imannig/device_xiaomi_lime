eb152d3b1da302b3092af7876cd4e24426937bf2
imannig <zahranansaria@gmail.com>
2023-04-21 05:28:38 +0700

    lime: Adapt to ProjectElixir
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- aosp_lime.mk
+++ aosp_lime.mk
@@ -8,17 +8,28 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common StatiXOS stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common ProjectElixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from lime device
 $(call inherit-product, device/xiaomi/lime/device.mk)
 
-PRODUCT_NAME := statix_lime
+PRODUCT_NAME := aosp_lime
 PRODUCT_DEVICE := lime
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi 9T
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Project Elixir Stuff
+IS_PHONE := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := rriaame
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_QUICK_TAP := true
