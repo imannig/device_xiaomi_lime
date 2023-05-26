fa40efa63954da1717ba0712f9907f105689f655
imannig <zahranansaria@gmail.com>
2023-05-26 09:06:04 +0700

    lime: vendorsetup: Add bengal hals
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,5 @@ git clone https://github.com/frstprjkt/kernel_xiaomi_sm6115 kernel/xiaomi/sm6115
 git clone https://github.com/imannig/device_xiaomi_sm6115 device/xiaomi/sm6115-common 
 git clone https://github.com/imannig/vendor_xiaomi_lime vendor/xiaomi/lime
 git clone https://github.com/frstprjkt/device_xiaomi_sm6115-common-kernel device/xiaomi/sm6115-common-kernel
+
+git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio hardware/qcom-caf/bengal/audio && git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_media hardware/qcom-caf/bengal/media && git clone https://github.com/PixelExperience/hardware_qcom-caf_bengal_display hardware/qcom-caf/bengal/display
